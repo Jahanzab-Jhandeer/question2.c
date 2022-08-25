fe7e09c189d0837df0f9db9d1990078106779dc0
Jahanzab-Jhandeer <74298308+Jahanzab-Jhandeer@users.noreply.github.com>
2022-08-25 05:38:52 -0700

    Write a program in C to Print Fibonacci Series using recursion

---
+++ q2.c
@@ -0,0 +1,26 @@
+ #include<stdio.h>   
+ 
+ // Write a program in C to Print Fibonacci Series using recursion
+ 
+ void printFibonacci(int n);
+
+int main(){    
+    int n;    
+    printf("Enter the number of elements: ");    
+    scanf("%d",&n);    
+    printf("Fibonacci Series: ");    
+    printf("%d %d ",0,1);    
+    printFibonacci(n-2);    //n-2 because 2 numbers are already printed    
+  return 0;  
+ }    
+
+ void printFibonacci(int n){    
+    static int n1=0,n2=1,n3;    
+    if(n>0){    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+         printf("%d ",n3);    
+         printFibonacci(n-1);    
+    }    
+}    
